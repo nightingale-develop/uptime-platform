89f6db0c4d47b3acaeb8a71ed9bc6dc9242f8df4
SashaSolovey1 <solo160103@gmail.com>
2026-09-18 18:29:48 +0200

    chore(release): v1.3.0

--- src/uptime_platform/main.py
+++ src/uptime_platform/main.py
@@ -41,7 +41,7 @@ from uptime_platform.status_pages.router import (
 
 app = FastAPI(
     title="Uptime Platform API",
-    version="1.2.1",
+    version="1.3.0",
 )
 
 web_settings = get_web_settings()
